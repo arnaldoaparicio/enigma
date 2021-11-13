deca68a061f0114de6cbc60a841241d9b8472e2a
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-13 11:22:16 -0500

    Add generate_key, generate_offset, and addition methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,22 +1,57 @@
 class Enigma
-attr_reader :alphabet, :key, :offset
-def initialize
-  @alphabet = ("a".."z").to_a << " "
-  @key = key
-  @offset = Date.today.strftime("%d%m%y")
+  attr_reader :alphabet, :key, :date
+
+  def initialize
+    @alphabet = ('a'..'z').to_a << ' '
+    @key = key
+    @date = date
+  end
+
+  # def todays_date
+  #   @offset = Date.today.strftime('%d%m%y')
+  # end
+
+  def generate_key(key)
+    list = []
+    a = key[0..1]
+    b = key[1..2]
+    c = key[2..3]
+    d = key[3..4]
+    list << a << b << c << d
+    list.map(&:to_i)
+  end
+
+  def generate_offset(date)
+    list = []
+    numeric = date.to_i**2
+    a = numeric.to_s[-4]
+    b = numeric.to_s[-3]
+    c = numeric.to_s[-2]
+    d = numeric.to_s[-1]
+    list << a << b << c << d
+    list.map(&:to_i)
+  end
+
+  def addition(key, date)
+    @key = generate_key(key)
+    @date = generate_offset(date)
+    @key.zip(@date).map {|x, y| x + y}
+  end
+
+  def encrypt(message, key = nil, date = nil)
+    e =  addition(key, date)
+    mess = message.split("")
+    mess.map do |m|
+      
+      # require "pry"; binding.pry
+    # end
 
 
+
+   end
 end
 
-def random_number_generator
-  @key = 5.times.map {rand(0..9)}.join
-end
-
-
-# def encrypt(message, key, date)
-# end
 #
 #
 # def decrypt(message, key, date)
 # end
-end
