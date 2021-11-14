201089485dbd71e6cea9485c455285e7d2adc667
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-14 18:55:45 -0500

    Refactor encrypt method to include if statements in the event that key and date are nil

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,11 @@
 class Enigma
-  attr_reader :alphabet, :key, :date
+  attr_reader :alphabet, :keys, :dates, :random
 
   def initialize
     @alphabet = ('a'..'z').to_a << ' '
-    @key = key
-    @date = date
+    @keys = keys
+    @dates = dates
+    @random = []
   end
 
   # def todays_date
@@ -33,17 +34,28 @@ class Enigma
   end
 
   def addition(key, date)
-    @key = generate_key(key)
-    @date = generate_offset(date)
-    @key.zip(@date).map { |x, y| x + y }
+    @keys = generate_key(key)
+    @dates = generate_offset(date)
+    @keys.zip(@dates).map { |x, y| x + y }
   end
 
   def encrypt(message, key = nil, date = nil)
+
+
+    if date == nil
+      date = Date.today.strftime('%d%m%y')
+    end
+    if key == nil
+      @random << 5.times.map {rand(0..9)}.join
+      key = @random.join
+    end
+
     encryption_hash = {}
     all_encrypted = []
     all_keys = addition(key, date)
 
-    given_message = message.split('')
+
+    given_message = message.downcase.split('')
     given_message.each_with_index do |value, index|
       if index % 4 == 0
         a_shift = @alphabet.index(value) + all_keys[0]
@@ -53,7 +65,7 @@ class Enigma
         all_encrypted << @alphabet[b_shift % 27]
       elsif index % 4 == 2
         c_shift = @alphabet.index(value) + all_keys[2]
-        all_encrypted << alphabet[c_shift  % 27]
+        all_encrypted << alphabet[c_shift % 27]
       elsif index % 4 == 3
         d_shift = @alphabet.index(value) + all_keys[3]
         all_encrypted << alphabet[d_shift % 27]
@@ -62,10 +74,38 @@ class Enigma
     encrypt = all_encrypted.join
     encryption_hash.merge!(encryption: encrypt, key: key, date: date)
   end
+
+  def decrypt(message, key = nil, date = nil)
+    decryption_hash = {}
+    all_decrypted = []
+
+
+    all_keys = addition(key, date)
+    if date == nil
+      date = Date.today.strftime('%d%m%y')
+    end
+    # if key == nil
+    #   @random.join
+    # end
+
+    given_message = message.downcase.split('')
+    given_message.each_with_index do |value, index|
+      if index % 4 == 0
+        a_shift = @alphabet.index(value) - all_keys[0]
+        all_decrypted << @alphabet[a_shift % 27]
+        
+      elsif index % 4 == 1
+        b_shift = @alphabet.index(value) - all_keys[1]
+        all_decrypted << @alphabet[b_shift % 27]
+      elsif index % 4 == 2
+        c_shift = @alphabet.index(value) - all_keys[2]
+        all_decrypted << @alphabet[c_shift % 27]
+      elsif index % 4 == 3
+        d_shift = @alphabet.index(value) - all_keys[3]
+        all_decrypted << @alphabet[d_shift % 27]
+      end
+    end
+    decrypt = all_decrypted.join
+    decryption_hash.merge!(decryption: decrypt, key: key, date: date)
+  end
 end
-
-
-#
-#
-# def decrypt(message, key, date)
-# end
