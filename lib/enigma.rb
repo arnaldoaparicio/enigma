340b1d7e085bf87b4bbf2fb05ea87376fcb86dca
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-13 22:01:24 -0500

    Add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -35,21 +35,35 @@ class Enigma
   def addition(key, date)
     @key = generate_key(key)
     @date = generate_offset(date)
-    @key.zip(@date).map {|x, y| x + y}
+    @key.zip(@date).map { |x, y| x + y }
   end
 
   def encrypt(message, key = nil, date = nil)
-    e =  addition(key, date)
-    mess = message.split("")
-    mess.map do |m|
-      
-      # require "pry"; binding.pry
-    # end
+    encryption_hash = {}
+    all_encrypted = []
+    all_keys = addition(key, date)
 
-
-
-   end
+    given_message = message.split('')
+    given_message.each_with_index do |value, index|
+      if index % 4 == 0
+        a_shift = @alphabet.index(value) + all_keys[0]
+        all_encrypted << @alphabet[a_shift % 27]
+      elsif index % 4 == 1
+        b_shift = @alphabet.index(value) + all_keys[1]
+        all_encrypted << @alphabet[b_shift % 27]
+      elsif index % 4 == 2
+        c_shift = @alphabet.index(value) + all_keys[2]
+        all_encrypted << alphabet[c_shift  % 27]
+      elsif index % 4 == 3
+        d_shift = @alphabet.index(value) + all_keys[3]
+        all_encrypted << alphabet[d_shift % 27]
+      end
+    end
+    encrypt = all_encrypted.join
+    encryption_hash.merge!(encryption: encrypt, key: key, date: date)
+  end
 end
+
 
 #
 #
