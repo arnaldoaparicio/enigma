4777165914743c7e33e08954117404217e6a813e
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-16 00:28:40 -0500

    Edit local variable to adhere to proper naming conventions

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -8,10 +8,10 @@ new_file = File.open(ARGV[0], 'r')
 encrypt_file = new_file.read
 new_file.close
 
-example = enigma.encrypt(encrypt_file.strip)
+encrypted_message = enigma.encrypt(encrypt_file.strip)
 
 writer = File.open(ARGV[1], "w")
-writer.write(example[:encryption])
+writer.write(encrypted_message[:encryption])
 writer.close
 
-p "Created 'encrypted.txt' with the key #{example[:key]}, and date #{example[:date]}"
+p "Created 'encrypted.txt' with the key #{encrypted_message[:key]}, and date #{encrypted_message[:date]}"
